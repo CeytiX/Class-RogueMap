c3d0cdd51624ace07c427d02a1639e4909cee519
Ceytix <ceytix@mailoo.org>
2015-06-29 18:26:40 +0200

    third room

--- src/wall_map_generator.cpp
+++ src/wall_map_generator.cpp
@@ -196,6 +196,162 @@ int main()
 		}
 	}
 	
+	/*********
+	* ROOM 3 *
+	*********/
+
+	int xMin_3, xMax_3, yMin_3, yMax_3, width_3, height_3;
+
+	bool room_3(false);
+	bool corridor_3(true);
+	bool orientation_3;
+
+	while(!room_3)
+	{
+		width_3 = rand()%10 + 6;
+		height_3 = rand()%10 + 6;
+
+		xMin_3 = rand()%(39-width_3);
+		yMin_3 = rand()%(39-height_3);
+
+		xMax_3 = xMin_3 + width_3;
+		yMax_3 = yMin_3 + height_3;
+
+		bool intersection = true;
+		
+		for(int i=(xMin_3-2>=0)?xMin_3-2:xMin_3; i<=(xMax_3+2<40?xMax_3+2:xMax_3); i++)
+		{
+			for(int j=(yMin_3-2>=0)?yMin_3-2:yMin_3; j<=(yMax_3+2<40?yMax_3+2:yMax_3); j++)
+			{
+				if(map[i][j] > 0)
+				{
+					intersection = false;
+				}
+			}
+		}
+		
+		bool placed = false;
+		
+		if((xMax_3>=xMin_2+3 && xMax_2>=xMin_3+3) || (yMax_3>=yMin_2+3 && yMax_2>=yMin_3+3))
+		{
+			placed = true;
+		}
+		
+		/******************
+		* ROOM.corridor 3 *
+		******************/
+	
+		if(xMax_3>=xMin_2+3 && xMax_2>=xMin_3+3)
+		{
+			int y = (yMin_2 > yMax_3)?yMax_3:yMax_2;
+			int y2 = (yMin_2 > yMax_3)?yMin_2:yMin_3;
+			int x = (xMax_2 < xMax_3)?xMax_2:xMax_3;
+		
+			for(int i=y+1; i<y2; i++)
+			{
+				if(map[x][i]>0 || map[x-2][i]>0)
+				{
+					corridor_3 = false;
+					orientation_3 = true;
+				}
+			}
+		}
+		else
+		{
+			int x = (xMin_2 > xMax_3)?xMax_3:xMax_2;
+			int x2 = (xMin_2 > xMax_3)?xMin_2:xMin_3;
+			int y = (yMax_2 < yMax_3)?yMax_2:yMax_3;
+		
+			for(int i=x+1; i<x2; i++)
+			{
+				if(map[i][y]>0 || map[i][y-2]>0)
+				{
+					corridor_3 = false;
+					orientation_3 = false;
+				}
+			}
+		}
+		
+		room_3 = intersection && placed && corridor_3;
+	}
+	
+	/**************
+	* ROOM.place 3 *
+	**************/
+	
+	/** place the walls **/
+	
+	for(int i=xMin_3; i<=xMax_3; i++) 
+	{
+		map[i][yMin_3]++;
+		map[i][yMax_3]++;
+	}
+
+	for(int j=yMin_3+1; j<yMax_3; j++)
+	{
+		map[xMin_3][j]++;
+		map[xMax_3][j]++;
+	}
+	
+	/** place the corridor **/
+	
+	if(xMax_3>=xMin_2+3 && xMax_2>=xMin_3+3)
+	{
+		int y = (yMin_2 > yMax_3)?yMax_3:yMax_2;
+		int y2 = (yMin_2 > yMax_3)?yMin_2:yMin_3;
+		int x = (xMax_2 < xMax_3)?xMax_2:xMax_3;
+
+		for(int i=y; i<=y2; i++)
+		{
+			map[x][i]++;
+			map[x-2][i]++;
+		}
+	}
+	else
+	{
+		int x = (xMin_2 > xMax_3)?xMax_3:xMax_2;
+		int x2 = (xMin_2 > xMax_3)?xMin_2:xMin_3;
+		int y = (yMax_2 < yMax_3)?yMax_2:yMax_3;
+
+		for(int i=x; i<=x2; i++)
+		{
+			map[i][y]++;
+			map[i][y-2]++;
+		}
+	}
+	
+	/** place the doors **/
+	
+	for(int i=0; i<40; i++)
+	{
+		for(int j=0; j<40; j++)
+		{
+			if(map[i][j] == 2)
+			{
+				if(map[i-2][j] == 2 && map[i-1][j] == 1 && orientation_3)
+				{
+					map[i][j] = map[i-2][j] = 1;
+					map[i-1][j] = 3;
+				}
+				else if(map[i+2][j] == 2 && map[i+1][j] == 1 && orientation_3)
+				{
+					map[i][j] = map[i+2][j] = 1;
+					map[i+1][j] = 3;
+				}
+				else if(map[i][j-2] == 2 && map[i][j-1] == 1 && !orientation_3)
+				{
+					map[i][j] = map[i][j-2] = 1;
+					map[i][j-1] = 4;
+				}
+				else if(map[i][j+2] == 2 && map[i][j+1] == 1 && !orientation_3)
+				{
+					map[i][j] = map[i][j+2] = 1;
+					map[i][j+1] = 4;
+				}
+			}
+		}
+	}
+	
 	/********
 	* PRINT *
 	********/
